dbdc119a6b1f8419684c02078050ce0963093541
pavangpv <94555758+pavangpv@users.noreply.github.com>
2021-12-14 16:40:03 +0530

    Delete extensions directory

--- extensions/tock.vscode-css-custom-properties-0.0.5/out/ReferenceProvider.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CustomPropertyReferenceProvider {
-    constructor(customPropertyManager) {
-        this.manager = customPropertyManager;
-    }
-}
-exports.default = CustomPropertyReferenceProvider;
-//# sourceMappingURL=ReferenceProvider.js.map
